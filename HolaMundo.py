e46702afa1a2f4ca6a0493a8490e8adbb5cb66e0
AlanMarin <alan_tigre_02@hotmail.com>
2020-02-26 15:25:57 +0000

    Initial commit

---
+++ HolaMundo.py
@@ -0,0 +1,8 @@
+# holamundo típico. Muestra un simple mensaje, para ilustrar la estructrura
+# típica de un programa.
+
+# El punto de entrada se llamará main
+# Se compone de un estatuto def
+
+def main():
+  print("Hola mundo, ahora en Python")
